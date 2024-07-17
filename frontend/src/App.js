d494a31691d66236bbcf2ccf9897a394a0afcfd9
CesarNr <cesar.inuncira@gmail.com>
2024-07-17 19:05:20 -0400

    create-React-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/" >Vit & Fit</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
